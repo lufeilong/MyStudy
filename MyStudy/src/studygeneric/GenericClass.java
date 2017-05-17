7d505f2f3fefdf8cc85e6eda22afed422d9882ca
lufeilong <18610945021@163.com>
2017-05-17 15:27:12 +0800

    generic exer

---
+++ MyStudy/src/studygeneric/GenericClass.java
@@ -0,0 +1,25 @@
+package studygeneric;
+
+public class GenericClass<T> {
+	
+	private T t;
+
+	public void add(T t) {
+		this.t = t;
+	}
+
+	public T get() {
+		return t;
+	}
+
+	public static void main(String[] args) {
+		GenericClass<Integer> integerBox = new GenericClass<Integer>();
+		GenericClass<String> stringBox = new GenericClass<String>();
+
+		integerBox.add(new Integer(10));
+		stringBox.add(new String("菜鸟教程"));
+
+		System.out.printf("整型值为 :%d\n\n", integerBox.get());
+		System.out.printf("字符串为 :%s\n", stringBox.get());
+	}
+}
